9819f5a962dc2869b7ec9967b64789472d95da92
SreehariRS <sreeharikannan373@gmail.com>
2025-04-21 16:11:03 +0530

    cors fixed

--- src/app.ts
+++ src/app.ts
@@ -5,9 +5,20 @@ import imageRoutes from './routes/imageRoutes';
 
 const app = express();
 
+const allowedOrigins = [
+  'http://localhost:3000',
+  'https://image-gallery-frontend-e7bfj87ef-sreeharirs-projects.vercel.app'
+];
+
 app.use(cors({
-  origin: 'http://localhost:3000',
-  credentials: true,
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true
 }));
 app.use(express.json());
 app.use('/uploads', express.static('uploads'));
